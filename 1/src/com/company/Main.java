eaf12ed35c8c1541090072d81898f20e36d83313
Darkerrus <56172007+Darkerrus@users.noreply.github.com>
2021-02-23 18:25:43 +0300

    Add files via upload

---
+++ 1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Predicate<Integer> predicate = x -> (x != 0) && ((x & (x - 1)) == 0);
+	System.out.println(predicate.test(128));
+    }
+
+
+}
